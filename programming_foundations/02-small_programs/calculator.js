c42118f9b01414caba74e46de5985f2a421599d6
Tom Cobb <tom@tomcobbcodes.com>
2022-04-19 21:54:30 -0400

    Updated calculator to use JSON for language config

--- programming_foundations/02-small_programs/calculator.js
+++ programming_foundations/02-small_programs/calculator.js
@@ -4,32 +4,43 @@
 // perform the operation
 // display the result of the operation
 
-const readline = require('readline-sync');
+const READLINE = require('readline-sync');
+const MESSAGES = require('./calculator_messages.json');
 
 function prompt(msg) {
   console.log(`\n=> ${msg}`);
 }
 
 function errorPrompt(msg) {
-  console.log(`\n~~ERROR~~=> ${msg}\n`);
+  console.log(`${MESSAGES.error} ${msg}`);
 }
 
-console.log(" _____________________");
-console.log("|  _________________  |");
-console.log("| |   *BEEP BOOP*   | |");
-console.log("| |_________________| |");
-console.log("|  ___ ___ ___   ___  |");
-console.log("| | 7 | 8 | 9 | | + | |");
-console.log("| |___|___|___| |___| |");
-console.log("| | 4 | 5 | 6 | | - | |");
-console.log("| |___|___|___| |___| |");
-console.log("| | 1 | 2 | 3 | | x | |");
-console.log("| |___|___|___| |___| |");
-console.log("| | . | 0 | = | | / | |");
-console.log("| |___|___|___| |___| |");
-console.log("|_____________________|");
+function getLanguage() {
+  let selection = "";
+  while (selection === "") {
+    selection = READLINE.question(prompt(MESSAGES.language_query));
+    switch (selection) {
+      case "1":
+        selection = "english";
+        break;
+      case "2":
+        selection = "spanish";
+        break;
+      default:
+        prompt(MESSAGES.language_error);
+        selection = "";
+    }
+  }
+  return selection;
+}
 
-prompt('*BEEP BOOP* Welcome to the calcul8r!');
+let language = getLanguage();
+
+MESSAGES[language].welcome_img.forEach(elem => {
+  console.log(elem);
+});
+
+prompt(MESSAGES[language].welcome_msg);
 
 let continuePlaying = true;
 
@@ -37,17 +48,17 @@ while (continuePlaying) {
 
   let number1;
   do {
-    number1 = Number(readline.question(prompt('What is the first number?')));
+    number1 = Number(READLINE.question(prompt(MESSAGES[language].first_num)));
     if (isNaN(number1)) {
-      errorPrompt('That is not a valid number.');
+      errorPrompt(MESSAGES[language].invalid_num);
     }
   } while (isNaN(number1));
 
   let number2;
   do {
-    number2 = Number(readline.question(prompt('What is the second number?')));
+    number2 = Number(READLINE.question(prompt(MESSAGES[language].second_num)));
     if (isNaN(number2)) {
-      console.log(errorPrompt('That is not a valid number.'));
+      console.log(errorPrompt(MESSAGES[language].invalid_num));
     }
   } while (isNaN(number2));
 
@@ -55,7 +66,7 @@ while (continuePlaying) {
   let operation = '';
 
   while (operation === '') {
-    operation = readline.question(prompt('What operation would you like to perform?'));
+    operation = READLINE.question(prompt(MESSAGES[language].operation));
     switch (operation) {
       case '+':
         result = number1 + number2;
@@ -71,18 +82,23 @@ while (continuePlaying) {
         break;
       default:
         operation = '';
-        console.log(errorPrompt("Please choose from the following operations: + - * /"));
+        console.log(errorPrompt(MESSAGES[language].operation_error));
     }
   }
 
   console.log(`\n${number1} ${operation} ${number2} = ${result}`);
 
   while (true) {
-    continuePlaying = Number(readline.question(prompt('Would you like to continue playing?\nEnter 1 for yes. Enter 0 for no.')));
+    continuePlaying = Number(
+      READLINE.question(prompt(MESSAGES[language].continue))
+    );
     if (continuePlaying === 1 || continuePlaying === 0) {
       break;
+    } else if (continuePlaying === 2) {
+      language = getLanguage();
+      break;
     } else {
-      errorPrompt("Please enter 1 for yes or 0 for no.");
+      errorPrompt(MESSAGES[language].continue_error);
     }
   }
 
